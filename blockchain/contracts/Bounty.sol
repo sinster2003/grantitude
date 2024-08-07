b975dea3a1c45b3bd9d0ab84eed22816cf35d8ab
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-08-08 03:45:15 +0530

    rough sketch of bounty contract

---
+++ blockchain/contracts/Bounty.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bounty {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    event BountyAwarded(address payable _repoContributor);
+    
+    // transfer funds from repoOwner to repoContributor
+    function awardBounty(address payable _repoContributor) public payable {
+        require(msg.value > 0, "Bounty has to greater than 0 ethers")
+        require(msg.value <= msg.sender.balance, "Not enough balance present to award the bounty");
+        (bool status, ) = _repoContributor.call{value: msg.value}("");
+        require(status, "Transaction failed");
+        emit BountyAwarded(_repoContributor);
+    }
+}
--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,11 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {}
+  }
 };
 
 export default config;
